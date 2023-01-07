69b15bf7dda4641b16c0955488634d4fca568f31
blazhkevych.v <blazhkevych.v@gmail.com>
2023-01-07 18:55:31 +0200

    ц

--- task/MainWindow.xaml.cs
+++ task/MainWindow.xaml.cs
@@ -1,139 +1,126 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace task
+namespace task;
+// Приложение "Блокнот"
+// Разработать приложение «Блокнот», обладающее той же функциональностью,
+// что и стандартный «Блокнот» операционной системы Windows.
+
+// Notepad application
+// Develop a notepad application that has the same functionality
+// as the standard Notepad Windows operating system.
+public partial class MainWindow : Window
 {
-    // Приложение "Блокнот"
-    // Разработать приложение «Блокнот», обладающее той же функциональностью,
-    // что и стандартный «Блокнот» операционной системы Windows.
-
-    // Notepad application
-    // Develop a notepad application that has the same functionality
-    // as the standard Notepad Windows operating system.
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-            // Window position in the center of the screen.
-            WindowStartupLocation = WindowStartupLocation.CenterScreen;
-        }
+        InitializeComponent();
+        // Window position in the center of the screen.
+        WindowStartupLocation = WindowStartupLocation.CenterScreen;
+    }
 
-        private void NewDocument_MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
+    private void NewDocument_MenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
 
-        private void NewWindow_MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
+    private void NewWindow_MenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
 
-        private void Open_MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
+    private void Open_MenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
 
-        private void Save_MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
+    private void Save_MenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
 
-        private void SaveAs_MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
+    private void SaveAs_MenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
 
-        private void PageSetup_MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
+    private void PageSetup_MenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
 
-        private void Exit_MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
+    private void Exit_MenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
 
-        private void Undo_MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
+    private void Undo_MenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
 
-        private void Cut_MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
+    private void Cut_MenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
 
-        private void Copy_MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
+    private void Copy_MenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
 
-        private void Paste_MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
+    private void Paste_MenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
 
-        private void Delete_MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
+    private void Delete_MenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
 
-        private void Replace_MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
+    private void Replace_MenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
 
-        private void SelectAll_MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
+    private void SelectAll_MenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
 
-        private void TimeDate_MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
+    private void TimeDate_MenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
 
-        private void ZoomIn_MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
+    private void ZoomIn_MenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
 
-        private void ZoomOut_MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
+    private void ZoomOut_MenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
 
-        private void ZoomRestore_MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
+    private void ZoomRestore_MenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
 
-        private void ShowStatusBar_MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
+    private void ShowStatusBar_MenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
 
-        private void WordWrap_MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
+    private void WordWrap_MenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
 
-        private void TextArea_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            //throw new NotImplementedException();
-        }
+    private void TextArea_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        //throw new NotImplementedException();
     }
 }
