2dd50708a1b87b55d0658729f425d65178960345
blazhkevych.v <blazhkevych.v@gmail.com>
2023-01-08 18:32:41 +0200

    ы

--- task/MainWindow.xaml.cs
+++ task/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using Microsoft.Win32;
 
 namespace task;
 // Приложение "Блокнот"
@@ -10,8 +14,13 @@ namespace task;
 // Notepad application
 // Develop a notepad application that has the same functionality
 // as the standard Notepad Windows operating system.
+
+// todo: add borders to the menu/submenu
 public partial class MainWindow : Window
 {
+    // Name of the current file.
+    private string NameOfTheCurrentFile { get; set; } = "Untitled";
+
     public MainWindow()
     {
         InitializeComponent();
@@ -19,24 +28,16 @@ public partial class MainWindow : Window
         WindowStartupLocation = WindowStartupLocation.CenterScreen;
     }
 
-    private void NewDocument_MenuItem_Click(object sender, RoutedEventArgs e)
-    {
-        throw new NotImplementedException();
-    }
-
-    private void NewWindow_MenuItem_Click(object sender, RoutedEventArgs e)
-    {
-        throw new NotImplementedException();
-    }
-
-    private void Open_MenuItem_Click(object sender, RoutedEventArgs e)
-    {
-        throw new NotImplementedException();
-    }
-
     private void Save_MenuItem_Click(object sender, RoutedEventArgs e)
     {
-        throw new NotImplementedException();
+        var dlg = new SaveFileDialog();
+        dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+        if (dlg.ShowDialog() == true)
+        {
+            var fileStream = new FileStream(dlg.FileName, FileMode.Create);
+            var range = new TextRange(TextEditor.Document.ContentStart, TextEditor.Document.ContentEnd);
+            range.Save(fileStream, DataFormats.Rtf);
+        }
     }
 
     private void SaveAs_MenuItem_Click(object sender, RoutedEventArgs e)
@@ -122,5 +123,37 @@ public partial class MainWindow : Window
     private void TextArea_TextChanged(object sender, TextChangedEventArgs e)
     {
         //throw new NotImplementedException();
+    }
+
+    private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
+    {
+        // todo: Add a question before opening: "Do you want to save changes to the file WITHOUT A NAME?"
+
+        OpenFileDialog openFile = new OpenFileDialog();
+        openFile.Filter = "Plain Text File (*.txt)|*.txt|Rich Text File (*.rtf)|*.rtf|All files (*.*)|*.*";
+        if (openFile.ShowDialog() == true)
+        {
+            var fileStream = new FileStream(openFile.FileName, FileMode.Open);
+            var range = new TextRange(TextEditor.Document.ContentStart, TextEditor.Document.ContentEnd);
+            range.Load(fileStream, DataFormats.Text);
+        }
+    }
+
+    private void Save_Executed(object sender, ExecutedRoutedEventArgs e)
+    {
+        SaveFileDialog saveFile = new SaveFileDialog();
+        saveFile.FileName = NameOfTheCurrentFile;
+        saveFile.Title = "Save";
+        saveFile.Filter = "Plain Text File (*.txt)|*.txt|Rich Text File (*.rtf)|*.rtf|All files (*.*)|*.*";
+
+        FileStream fileStream = new FileStream(saveFile.FileName, FileMode.CreateNew);
+        TextRange range = new TextRange(TextEditor.Document.ContentStart, TextEditor.Document.ContentEnd);
+        range.Save(fileStream, DataFormats.Text);
+
+    }
+
+    private void SaveAs_Executed(object sender, ExecutedRoutedEventArgs e)
+    {
+        throw new NotImplementedException();
     }
 }
